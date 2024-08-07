891248c242bcbe7b5a1f676a1168af27719a8400
NicolasRbelo <rabelonicolas01@gmail.com>
2024-08-07 15:37:13 -0300

    Adição de novos arquivos e atualização de outros.

--- js/command.js
+++ js/command.js
@@ -1,25 +1,41 @@
 /*====================== scroll sections active link ==============*/
+let IconeMenu = document.querySelector('#icone-menu')
+let BarraNav = document.querySelector('.barraNav')
+
+IconeMenu.onclick = () => {
+  IconeMenu.classList.toggle('bx-x');
+  BarraNav.classList.toggle('ativo');
+};
+
+/*====================== Troca de seção Ativa ==============*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
   sections.forEach(sec => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
+    let topo = window.scrollY;
+    let Defora = sec.offsetTop - 150;
+    let Altura = sec.offsetHeight;
     let id = sec.getAttribute('id');
 
-    if(top >= offset && top < offset + height) {
+    if(topo >= Defora && topo < Defora + Altura) {
       navLinks.forEach(links => {
         links.classList.remove('ativo');
         document.querySelector('header nav a[href*=' + id + ']').classList.add('ativo');
       });
     } 
   });
-  /*============================== sticky navbar ====================*/
+  /*==================================================*/
 
-    let cabeca = document.querySelector('header')
+    let cabecario = document.querySelector('header')
     
 
-    cabeca.classList.toggle('loginho', window.scrollY > 100)
-};
+    cabecario.classList.toggle('loginho', window.scrollY > 100)
+  /*====================== Menu fecha ao trocar de seção  ==============*/
+
+  IconeMenu.classList.remove('bx-x');
+  BarraNav.classList.remove('ativo');
+
+  
+  };
+
